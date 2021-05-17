68ab7338cc54ca3b11efbdc9fe5a1ac7d3d97938
achimcc <achim.schneider@posteo.de>
2021-05-17 19:31:22 +0200

    typization of useDispatch

--- src/modules/redux/store/store.ts
+++ src/modules/redux/store/store.ts
@@ -1,5 +1,5 @@
 import { createSelectorHook, useDispatch as _useDispatch } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { createEpicMiddleware } from 'redux-observable';
 import { createWrapper } from 'next-redux-wrapper';
 import logger from 'redux-logger';
@@ -20,14 +20,7 @@ const makeStore = () => {
   const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware =>
-      getDefaultMiddleware()
-        .prepend(
-          // correctly typed middlewares can just be used
-          epicMiddleware
-          // you can also type middlewares manually
-        )
-        // prepend and concat calls can be chained
-        .concat(logger),
+      getDefaultMiddleware().prepend(epicMiddleware).concat(logger),
   });
   epicMiddleware.run(rootEpic);
   return store;
@@ -41,9 +34,9 @@ function useSelector<T>(fn: (store: RootState) => T): T {
   return fn(_useSelector(x => x));
 }
 
-function useDispatch(): (args: any) => any {
-  const dispatch = _useDispatch();
-  return (action: any) => dispatch(action);
-}
+export type AppStore = ReturnType<typeof makeStore>;
+export type AppDispatch = AppStore['dispatch'];
+
+const useDispatch = () => _useDispatch<AppDispatch>();
 
 export { wrapper, useSelector, useDispatch };
