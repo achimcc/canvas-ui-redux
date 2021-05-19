e91eb0c2945d990aabe28a2e2979852f4fd1bb10
achimcc <achim.schneider@posteo.de>
2021-05-19 10:15:27 +0200

    fixed sidebar

--- src/common/components/Sidebar/SidebarItem.tsx
+++ src/common/components/Sidebar/SidebarItem.tsx
@@ -3,14 +3,15 @@ import { useRouter } from 'next/router';
 
 interface Props {
   href: string;
+  title: string;
   children?: any;
 }
 
-const Item = ({ href, children }: Props) => {
+const Item = ({ href, children, title }: Props) => {
   const router = useRouter();
   const isActive = href.startsWith(router.pathname);
   return (
-    <li className="h-14 border-b border-gray-900 hidden flex-1 sm:block" title="Home">
+    <li className="h-14 border-b border-gray-900 hidden flex-1 sm:block" title={title}>
       <div className={`h-full w-full hover:bg-gray-700 ${isActive && 'bg-white'} block p-3`}>
         <Link href={href}>{children}</Link>
       </div>
--- src/common/components/Sidebar/index.tsx
+++ src/common/components/Sidebar/index.tsx
@@ -1,31 +1,23 @@
 import type { BareProps as Props } from '@common/types';
-import { FaFileContract } from 'react-icons/fa';
-import { FiPlayCircle, FiUpload } from 'react-icons/fi';
-import { useSelector, selectors } from '../../../modules/redux';
-import ConnectIcon from '../global/ConnectIcon';
 import Item from './SidebarItem';
 
 export default function Sidebar(): React.ReactElement<Props> {
-  const status = useSelector(selectors.api.status);
-
   return (
     <>
       <nav>
         <div className="border-r-2 w-72 h-screen border-gray-700">
           <ul className="text-center flex flex-row justify-center sm:flex-col w-full">
-            <Item href="/connect">
-              <ConnectIcon status={status} />
+            <Item href="/connect" key="connect" title="Connect">
+              Connect
             </Item>
-            <Item href="/upload">
-              <div>
-                <FiUpload className="inline-block" />
-              </div>
+            <Item href="/upload" key="upload" title="Upload">
+              Upload
             </Item>
-            <Item href="/execute">
-              <FaFileContract className="inline-block" />
+            <Item key="execute" title="execute" href="/execute">
+              Execute
             </Item>
-            <Item href="/call">
-              <FiPlayCircle className="inline-block" />
+            <Item key="call" title="call" href="/call">
+              Call
             </Item>
           </ul>
         </div>
